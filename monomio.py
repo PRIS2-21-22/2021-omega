86915379399c46062cb67be9efef45675c735361
trm187 <trm187@inlumine.ual.es>
2021-03-21 18:39:45 +0100

    Clase monomio actualizada

--- monomio.py
+++ monomio.py
@@ -1,12 +1,16 @@
 class monomio:
 
+    #Constructor de la clase monomio
     def __init__(self, exponente, coeficiente):
         self.exponente = exponente
         self.coeficiente = coeficiente
 
+    #Función que llama a la clase monomio
+    def __call__(self):
+        return self
+
+    #Función que permite imprimir un monomio
+    def __str__(self):
+        return str(self.coeficiente)+"x^"+str(self.exponente)
 
 
-
-
-
-print('Hello world')
--- polinomio.py
+++ polinomio.py
@@ -1,5 +1,14 @@
-import sys
+from monomio import monomio
 
 class polinomio:
     f=open("input.txt", "r")
+
+    lines = f.readlines()
+
+    for line in lines:
+        values = line.split(", ")
+        print('exponente: {}, coeficiente: {}'.format(values[0], values[1]))
+        
+
     print(f.read())
+    print(monomio(1,3))
